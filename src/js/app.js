590cf8812e3a7988f891b1e9937824829dca530a
Adedayo Adebiyi <AAdebiyi@midmark.com>
2022-07-07 19:06:00 -0500

    fixed issue

--- src/js/app.js
+++ src/js/app.js
@@ -14,9 +14,12 @@
 
 
   App.prototype.fetchCurrentObservations = function() {
-   /*  var render = createRenderer("encounter");
-    render("Loading...");
-    return this.client.encounter.read().then(render, render); */
+
+    return this.client.observations.read().then((data)=>{
+      this.observations = data;
+    
+      console.log(this.observations);
+    });
   };
 /* 
   
@@ -35,15 +38,16 @@
   App.prototype.renderContext = function() {
     return Promise.all([
       this.fetchCurrentPatient(),
-      //this.fetchCurrentUser(),
+      this.fetchCurrentObservations(),
       //this.fetchCurrentEncounter()
     ]);
   };
   
 
   function renderPatient(patient){
-    const patientHTML = `<div class="w-100 d-flex flex-column p-5">
-              <h4>Name: ${patient.name}</h4>
+    const patientHTML = `<div class="w-100 d-flex flex-column p-5 justify-content-start">
+              <h1>Current Patient</h1>
+              <h4>Name: ${patient.name[0].given}  ${patient.name[0].family}</h4>
               <h4>Gender: ${patient.gender}</h4>
               <h4>${patient.birthDate}</h4>
             </div>`;
